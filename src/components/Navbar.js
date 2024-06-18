5bb36655d72a3250d06e04ff8e05de9b0aabf109
sandeepsinghh143 <sandeepsinghh143@gmail.com>
2024-06-18 22:17:27 +0530

    navbar padding added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ import React from "react";
 const Navbar = () => {
   const pathname = usePathname();
   return (
-    <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+    <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700 p-4">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto h-16">
         <Link
           href="#"
